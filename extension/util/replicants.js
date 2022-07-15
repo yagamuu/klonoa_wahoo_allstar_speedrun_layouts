da34394e5138c6ead9b6184123387513148e4a95
yagamuu <ygmpeer@gmail.com>
2022-07-15 21:21:30 +0900

    Add build files

---
+++ extension/util/replicants.js
@@ -0,0 +1,10 @@
+"use strict";
+/* eslint-disable max-len */
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * This is where you can declare all your replicant to import easily into other files,
+ * and to make sure they have any correct settings on startup.
+ */
+// YOU CAN REMOVE THIS RULE WHEN YOU GET MULTIPLE REPLICANTS!
+// eslint-disable-next-line import/prefer-default-export
+// export const exampleReplicant = nodecg().Replicant<ExampleReplicant>('exampleReplicant');
